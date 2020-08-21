bfb898812385326844f16388457cb62ee602042d
Jazmin Matos <jmatos1993@gmail.com>
2020-08-21 22:55:05 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,37 +1,44 @@
+#1
 def create_an_empty_array
   []
 end
 
-
+#2
 def create_an_array
   ["dog", "cat", "panda", "racoon"]
 end
 
-
+#3
 def add_element_to_end_of_array(array, element)
   ["dog", "cat", "panda", "racoon"]
 end
 
+#4
 def add_element_to_start_of_array(array, element)
   
 end
 
+#5
 def remove_element_from_end_of_array(array)
   
 end
 
+#6
 def remove_element_from_start_of_array(array)
   
 end
 
+#7
 def retrieve_element_from_index(array, index_number)
   
 end
 
+#8
 def retrieve_first_element_from_array(array)
   
 end
 
+#9
 def retrieve_last_element_from_array(array)
   
 end
